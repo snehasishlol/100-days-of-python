627815453e20743dea01747ce498f0dc73fc69f9
Snehasish Ray <snehasishcodes@gmail.com>
2023-03-26 08:47:37 +0000

    day #1 | completed

---
+++ days/1.py
@@ -0,0 +1,18 @@
+# python days/1.py
+# python3 days/1.py
+
+a = 10;
+b = 3.14;
+c = True;
+d = "hello world";
+e = [1, 2, 3, 4, 5, ["a", "b", "c"], [-1, -2], 5.5, 6.9, "a"];
+f = ("a", "b", "c", ("d", "e"), "f", ("g", "h"));
+g = { "hello": "world", "sixty": "nine" };
+
+print(type(a));
+print(type(b));
+print(type(c));
+print(type(d));
+print(type(e));
+print(type(f));
+print(type(g));
